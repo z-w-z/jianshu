6ecdc07cb753cc90468301d07bf75f7d993ea984
z-w-z <mr_zwzheng@163.com>
2020-02-17 23:59:20 +0800

    7-7使用combineReducers完成对数据的拆分管理

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -34,7 +34,7 @@ const  Header = (props) =>{
 
 const mapStateToProps = (state) => {
     return {
-        focused:state.focused,
+        focused:state.header.focused,
     }
 }
 const mapDispathToProps = (dispatch) => {
--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,18 +1,9 @@
-const defaulState = {
-    focused:false,
-}
+import { combineReducers } from 'redux';
+import  headerReducer  from '../common/header/store/reducer.js';
 
-export  default (state = defaulState,action)   =>{
-    if(action.type =="search_fucsed") {
-        const newState  = JSON.parse(JSON.stringify(state))
-        newState.focused = true;
-        return newState;
-    };
-    if(action.type =="search_blur") {
-        const newState  = JSON.parse(JSON.stringify(state))
-        newState.focused = false;
-        console.log(newState.focused)
-        return newState;
-    }
-    return state
- }
+
+const reducer = combineReducers({
+        header: headerReducer,
+});
+
+export default reducer;
