7fe4c54b14b86723b853704d9fcdb6addf75ddd4
z-w-z <mr_zwzheng@163.com>
2020-02-18 17:40:00 +0800

    7-11热门搜索样式布局

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,8 +1,48 @@
 import React from "react"
 import { connect } from 'react-redux'
-import { HeaderWrapper,Logo,Nav,NavItem,NavSearch ,Addition,Button ,SearchWrapper} from  './style'
+import {
+    HeaderWrapper,
+    Logo,
+    Nav,
+    NavItem,
+    NavSearch ,
+    Addition,
+    Button ,
+    SearchWrapper,
+    SearchInfo,
+    SearchInfoTitle,
+    SearchInfoSwitch,
+    SearchInfoList,
+    SearchInfoItem,
+} from  './style'
 import {actionCreators} from './store'
 const  Header = (props) =>{
+
+       const getList =(show) =>{
+        if(show){
+            return(
+                <SearchInfo>
+                    <SearchInfoTitle>
+                        热门搜索
+                        <SearchInfoSwitch>
+                            换一批
+                        </SearchInfoSwitch>
+                    </SearchInfoTitle>
+                    <SearchInfoList>
+                        <SearchInfoItem>教育</SearchInfoItem>
+                        <SearchInfoItem>教育</SearchInfoItem>
+                        <SearchInfoItem>教育</SearchInfoItem>
+                        <SearchInfoItem>教育</SearchInfoItem>
+                        <SearchInfoItem>教育</SearchInfoItem>
+                        <SearchInfoItem>教育</SearchInfoItem>
+                    </SearchInfoList>
+                </SearchInfo>
+            )
+
+        }else {
+            return null;
+        }
+    }
     return (
         <HeaderWrapper>
             <Logo href={"/"}/>
@@ -20,6 +60,7 @@ const  Header = (props) =>{
                         onBlur = {props.handleBlur}
                     ></NavSearch>
                     <span className = {props.focused ? 'focused iconfont' : 'iconfont'}>&#xe64d;</span>
+                    { getList(props.focused) }
                 </SearchWrapper>
                 <Addition >
                     <Button className='reg'>注册</Button>
