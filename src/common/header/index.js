eb9a4da67f29153a1bf06a8aa3fb7c52b892c9c3
z-w-z <mr_zwzheng@163.com>
2020-02-18 17:15:00 +0800

    7-10使用redux-immutable统一数据格式

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -35,7 +35,7 @@ const  Header = (props) =>{
 
 const mapStateToProps = (state) => {
     return {
-        focused:state.header.get("focused"),
+        focused:state.get("header").get("focused"),
     }
 }
 const mapDispathToProps = (dispatch) => {
