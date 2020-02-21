7cc73cce03a462c80808f76d4cad3f38c64a936f
z-w-z <mr_zwzheng@163.com>
2020-02-21 23:11:44 +0800

    8-1 什么是路由，如何在React中使用路由功能

--- src/App.js
+++ src/App.js
@@ -3,12 +3,23 @@ import logo from './logo.svg';
 import Header from './common/header'
 import './App.css';
 import { Provider } from 'react-redux'
+import { BrowserRouter,Route} from 'react-router-dom'
 import store from './store/index'
 
 function App() {
   return (
       <Provider store={store}>
-        <Header/>
+          <div>
+              <Header/>
+              <BrowserRouter>
+                  <div>
+                      <Route path='/' exact render={() =>(<div>home</div>)}></Route>
+                      <Route path='/detail' exact render={() =>(<div>detail</div>)}></Route>
+                  </div>
+
+              </BrowserRouter>
+          </div>
+
       </Provider>
 
 
