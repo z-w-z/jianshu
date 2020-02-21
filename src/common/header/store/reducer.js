fe0808fe37ee75b3c1f1e3010d44a4daaf40fca3
z-w-z <mr_zwzheng@163.com>
2020-02-21 23:11:17 +0800

    7-14热门搜索换页功能实现

--- src/common/header/store/reducer.js
+++ src/common/header/store/reducer.js
@@ -4,7 +4,10 @@ import {fromJS} from 'immutable'
 
 const defaulState = fromJS({
     focused:false,
-    list:[]
+    mouseIn:false,
+    list:[],
+    page:1,
+    totalPage:1,
 })
 
 export  default (state = defaulState,action)   =>{
@@ -14,7 +17,16 @@ export  default (state = defaulState,action)   =>{
         case constants.SEARCH_BLUR:
             return state.set('focused',false)
         case constants.CHANGE_LIST:
-            return state.set('list',action.data)
+            return state.merge({
+                list:action.data,
+                totalPage:action.totalPage
+            })
+        case constants.MOUSE_ENTER:
+            return state.set('mouseIn',true)
+        case  constants.MOUSE_LEAVE:
+            return state.set('mouseIn',false)
+        case  constants.CHANGE_PAGE:
+            return state.set('page',action.page)
         default:
             return state
     }
